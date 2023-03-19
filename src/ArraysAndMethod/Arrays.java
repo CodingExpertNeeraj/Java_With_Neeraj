5c0de4ea2dccd9358802a5c4f4b97e432e294c43
Neeraj tiwari <nt98924@gmail.com>
2023-03-19 14:11:47 +0530

    second way of define array

--- src/ArraysAndMethod/Arrays.java
+++ src/ArraysAndMethod/Arrays.java
@@ -4,12 +4,22 @@ public class Arrays {
     public static void main(String[] args) {
 //        int[] marks = {49, 59, 76, 64, 34};
 //        System.out.println(marks);
-        int[] marks = new int[3];
-        marks[0] = 68;
-        marks[1] = 48;
-        marks[2] = 69;
-        System.out.println(marks[0]);
-        System.out.println(marks[1]);
-        System.out.println(marks[2]);
+//        int[] marks = new int[3];
+//        marks[0] = 68;
+//        marks[1] = 48;
+//        marks[2] = 69;
+
+//        2nd way to define a array
+        int marks[] = {43,54,76};
+//        System.out.println(marks[0]);
+//        System.out.println(marks[1]);
+//        System.out.println(marks[2]);
+
+//        how to print the marks with loop
+        for (int i = 0; i < 3; i++){
+            System.out.println(marks[i]);
+        }
     }
 }
+
+// A variable takes 4 bite.
---
+++ src/ArraysAndMethod/SecondWayToDefineArray.java
@@ -0,0 +1,11 @@
+package ArraysAndMethod;
+
+public class SecondWayToDefineArray {
+    public static void main(String[] args) {
+        int[] marks = {78,54,64,84};
+
+        for (int i = 0; i < 4; i++) {
+            System.out.println(marks[i]);
+        }
+    }
+}
